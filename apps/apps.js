e621528c6dce2a1f480806eed3df195cb0c4fa56
dthrasher90 <dthrasher90@GMAIL.COM>
2016-08-08 10:00:03 -0800

    JSON practice
    json practice

---
+++ apps/apps.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+$(function(){
+  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    showResults(data.Search);
+  });
+});
+
+function showResults(results){
+  $.each(results, function(index,value){
+    console.log(value.Title);
+  });
+$('#search-result').html(html);
+}
